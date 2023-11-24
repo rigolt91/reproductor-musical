b7346c6dc5a8b19b939700f95697f80b06ecc58f
Rigoberto Carmenate Mayo <rigolt91@gmail.com>
2023-11-24 14:05:26 -0500

    [Feat] Add partial desing for new playlist

--- resources/js/Pages/NewPlaylist.jsx
+++ resources/js/Pages/NewPlaylist.jsx
@@ -1,6 +1,7 @@
 import NavLink from '@/Components/NavLink';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from "@inertiajs/react";
+import { Card, CardBody, CardHeader } from '@nextui-org/react';
 
 export default function NewPlaylist({ auth }) {
     return(
@@ -17,8 +18,15 @@ export default function NewPlaylist({ auth }) {
 
             <div className="py-8">
                 <div className="max-w-12xl mx-auto px-6 sm:px-6 lg:px-6">
-                    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
-
+                    <div className="grid grid-cols-1 md:grid-cols-2">
+                        <Card radius="sm" shadow="sm" className="w-full">
+                            <CardHeader className="border-b">
+                                <h3 className="text-md font-bold text-gray-800">Archivos Visuales</h3>
+                            </CardHeader>
+                            <CardBody>
+                                dd
+                            </CardBody>
+                        </Card>
                     </div>
                 </div>
             </div>
