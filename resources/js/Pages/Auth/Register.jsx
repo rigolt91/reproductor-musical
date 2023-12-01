41322c8fed09c24f9d16f362b25301ca5e2fee2a
Rigoberto Carmenate Mayo <rigolt91@gmail.com>
2023-12-01 13:41:17 -0500

    [Fix] Refactory

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,7 +1,5 @@
 import { useEffect } from 'react';
 import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
 import { Head, Link, useForm } from '@inertiajs/react';
